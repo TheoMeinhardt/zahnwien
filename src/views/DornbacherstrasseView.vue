8c93ba8dcc05c8f44bb3a968ce2bc5f38e7e7235
TheoMeinhardt <theo.meinhardt@batinfo.at>
2025-09-30 15:09:30 +0200

    Add NavBar import and include in Dornbacherstraße view

--- src/views/DornbacherstrasseView.vue
+++ src/views/DornbacherstrasseView.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
+    <NavBar />
+
     <h1>Dornbacherstraße 1</h1>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NavBar from '@/components/NavBar.vue'
+</script>
 
 <style scoped></style>
