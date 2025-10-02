3b541d19a236562d3265be9a322ef5d105a539ca
TheoMeinhardt <theo.meinhardt@batinfo.at>
2025-10-02 16:17:58 +0200

    feat: add ChipComponent for reusable chip UI element

---
+++ src/components/ChipComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-chip square :clickable="false" :color="props.color" :text-color="props.textColor" size="1rem">
+    <q-avatar size="sm"> <img :src="props.img" /> </q-avatar>
+    <slot></slot>
+  </q-chip>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  img: string
+  color: string
+  textColor: string
+}>()
+</script>
