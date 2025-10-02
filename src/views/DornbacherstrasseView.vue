4f132033f22cd2641e03438e7a4362319d120183
TheoMeinhardt <theo.meinhardt@batinfo.at>
2025-10-02 14:20:57 +0200

    feat: integrated navbar and added heading

--- src/views/DornbacherstrasseView.vue
+++ src/views/DornbacherstrasseView.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
-    <h1>Dornbacherstraße 1</h1>
+    <NavBar background="dark" text="white" :sticky="true" :overlay="false" />
+
+    <div class="inter text-primary q-mt-xl q-pt-xl q-pl-lg">
+      <span class="text-h2 text-weight-bold block">Dornbacherstraße 1</span>
+      <span class="text-h6 text-italic text-weight-regular">1170 Wien</span>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NavBar from '@/components/NavBar.vue'
+</script>
 
 <style scoped></style>
