92316920b1650941bfd4bb3e4cacb868079efd71
TheoMeinhardt <theo.meinhardt@batinfo.at>
2025-10-04 23:00:54 +0200

    feat: add ServiceBox component for displaying services

---
+++ src/components/ServiceBox.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="box row">
+    <div class="col q-mt-xl q-mb-md q-mx-lg">
+      <span class="inter text-primary text-h5 block">{{ props.header }}</span>
+      <span class="roboto text-dark text-subtitle2 text-weight-regular block q-mt-sm">
+        {{ props.subtext }}</span
+      >
+      <q-btn
+        flat
+        no-caps
+        color="primary"
+        icon-right="keyboard_arrow_right"
+        label="mehr erfahren"
+        size="md"
+        class="float-right q-mt-lg"
+      />
+    </div>
+
+    <q-img class="col full-width contain" :src="props.image"></q-img>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  header: string
+  subtext: string
+  image: string
+}>()
+</script>
+
+<style lang="scss" scoped>
+.box {
+  border: $primary 2px solid;
+  border-radius: 30px;
+}
+
+.q-img {
+  border-radius: 0px 30px 30px 0px;
+}
+</style>
