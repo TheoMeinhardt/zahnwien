63ace4366c249ae5349db409617ba3da37eee902
TheoMeinhardt <theo.meinhardt@batinfo.at>
2025-11-12 12:57:56 +0100

    feat: add map to DornbacherstrasseView component

--- src/views/DornbacherstrasseView.vue
+++ src/views/DornbacherstrasseView.vue
@@ -9,52 +9,59 @@
         <span class="text-h6 text-italic text-weight-regular">1170 Wien</span>
       </div>
 
-      <div>
-        <!-- Focus chips -->
-        <div>
-          <!-- Focus heading -->
-          <span class="text-h5 text-bold text-primary q-mt-lg block">Fokus:</span>
+      <div class="row">
+        <div class="col">
+          <!-- Focus chips -->
+          <div>
+            <!-- Focus heading -->
+            <span class="text-h5 text-bold text-primary q-mt-lg block">Fokus:</span>
 
-          <div class="inter row text-italic text-weight-light q-mt-sm">
-            <Chip color="white" text-color="primary" img="src/assets/icons/teeth_medicin.png"
-              >Allgemeine Zahnmedizin</Chip
-            >
+            <div class="inter row text-italic text-weight-light q-mt-sm">
+              <Chip color="white" text-color="primary" img="src/assets/icons/teeth_medicin.png"
+                >Allgemeine Zahnmedizin</Chip
+              >
 
-            <Chip color="white" text-color="primary" img="src/assets/icons/teeth_implants.png"
-              >Implantologie</Chip
-            >
+              <Chip color="white" text-color="primary" img="src/assets/icons/teeth_implants.png"
+                >Implantologie</Chip
+              >
 
-            <Chip color="white" text-color="primary" img="src/assets/icons/teeth_aesthetic.png"
-              >Ästhetik</Chip
-            >
+              <Chip color="white" text-color="primary" img="src/assets/icons/teeth_aesthetic.png"
+                >Ästhetik</Chip
+              >
+            </div>
+          </div>
+
+          <!-- Equipment chips -->
+          <div>
+            <!-- Equipment heading -->
+            <span class="text-h5 text-bold text-primary q-mt-lg block">Ausstattung:</span>
+
+            <div class="inter row text-italic text-weight-light q-mt-sm">
+              <Chip color="white" text-color="primary" img="src/assets/icons/x-ray.png"
+                >Röntgen</Chip
+              >
+
+              <Chip color="white" text-color="primary" img="">DVT</Chip>
+
+              <Chip color="white" text-color="primary" img="src/assets/icons/digital_workflow.png"
+                >digitale Workflows</Chip
+              >
+
+              <Chip color="white" text-color="primary" img="src/assets/icons/3d-scan.png"
+                >3D-Scan</Chip
+              >
+            </div>
+          </div>
+
+          <!-- Opening Hours -->
+          <div>
+            <!-- Opening Hours heading -->
+            <span class="text-h5 text-bold text-primary q-mt-lg block">Öffnungszeiten:</span>
           </div>
         </div>
 
-        <!-- Equipment chips -->
-        <div>
-          <!-- Equipment heading -->
-          <span class="text-h5 text-bold text-primary q-mt-lg block">Ausstattung:</span>
-
-          <div class="inter row text-italic text-weight-light q-mt-sm">
-            <Chip color="white" text-color="primary" img="src/assets/icons/x-ray.png">Röntgen</Chip>
-
-            <Chip color="white" text-color="primary" img="">DVT</Chip>
-
-            <Chip color="white" text-color="primary" img="src/assets/icons/digital_workflow.png"
-              >digitale Workflows</Chip
-            >
-
-            <Chip color="white" text-color="primary" img="src/assets/icons/3d-scan.png"
-              >3D-Scan</Chip
-            >
-          </div>
-        </div>
-
-        <!-- Opening Hours -->
-        <div>
-          <!-- Opening Hours heading -->
-          <span class="text-h5 text-bold text-primary q-mt-lg block">Öffnungszeiten:</span>
-        </div>
+        <!-- Map -->
+        <div id="map" class="col"></div>
       </div>
 
       <div class="q-mt-xl">
@@ -81,10 +88,28 @@ import NavBar from '@/components/NavBar.vue'
 import Chip from '@/components/ChipComponent.vue'
 import ServiceBox from '@/components/ServiceBox.vue'
 
+import L from 'leaflet'
+import { onMounted, ref, type Ref } from 'vue'
+
 import type { locationData } from '@/types'
 import rawData from '@/assets/data/1190/text.json'
 
-const data: locationData[] = rawData
+const data: Ref<locationData[]> = ref(rawData)
+const map: Ref<L.Map | undefined> = ref(undefined)
+const mapMarker: Ref<L.Marker | undefined> = ref(undefined)
+
+onMounted(() => {
+  map.value = L.map('map').setView([48.22625, 16.3075], 16)
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 25,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map.value)
+  mapMarker.value = L.marker([48.226278, 16.307361], {}).addTo(map.value)
+})
 </script>
 
-<style scoped></style>
+<style scoped>
+#map {
+  height: 20rem;
+}
+</style>
