392660aa1fe79e5a4ca45c90353d2bba0c4d2d7c
Luis Meinhardt <luis.meinhardt.wien@gmail.com>
2025-11-29 15:39:02 +0100

    About us Doktornamen geändert

--- src/views/AboutUsView.vue
+++ src/views/AboutUsView.vue
@@ -15,13 +15,13 @@
   <div class="soloPics">
 
     <div class="person card">
-      <p class="name">Dr. Isabele Olivia</p>
+      <p class="name">Dr. Isabelle Olivia</p>
       <img src="img/Uberuns/isabele.jpg" class="soloImg" />
       <p class="bio">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
     </div>
 
     <div class="person card">
-      <p class="name">Dr. Eva Olivia</p>
+      <p class="name">Dr. Ewa Olivia</p>
       <img src="img/Uberuns/Eva.jpg" class="soloImg" />
       <p class="bio">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
     </div>
