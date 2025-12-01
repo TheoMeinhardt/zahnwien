b6dc6914011284fa668058b9cae5f236d56177ba
TheoMeinhardt <theo.meinhardt@batinfo.at>
2025-12-01 17:11:27 +0100

    WIP: Style comprehensive Privacy Policy in PrivacyPolicyView component

--- src/views/PrivacyPolicyView.vue
+++ src/views/PrivacyPolicyView.vue
@@ -1,5 +1,134 @@
 <template>
-  <div>Privacy Policy</div>
+  <div class="q-pa-xl text-body1">
+    <span class="text-h3 block">Datenschutzerklärung der Ordination Dr. Olivia</span>
+    Der Schutz Ihrer persönlichen Daten ist uns ein besonderes Anliegen. Ihre Daten werden daher
+    ausschließlich auf Grundlage der gesetzlichen Bestimmungen der Europäischen
+    DatenSchutzGrundVerOrdnung (DSGVO), sowie des österreichischen Datenschutzgesetzes (DSG) in der
+    jeweils gültigen Fassung verarbeitet und gespeichert.
+    <br />
+    <br />
+    Diese Datenschutzinformation dient ausschließlich dazu, Sie über die wichtigsten Aspekte der
+    Datenverarbeitung Ihrer zur Verfügung gestellten Daten zu informieren.
+    <br />
+    <br />
+
+    <span class="text-h4 block">Datennutzung und Datenspeicherung</span>
+    Ihre Daten werden grundsätzlich nur von befugten Mitarbeitern der Ordination von Dr. Olivia
+    genutzt und dienen ausschließlich der Kontaktaufnahme und Bearbeitung Ihrer Anfragen und
+    Aufträge. Ihre Daten werden ohne Ihre Einwilligung nicht weitergegeben.
+    <br />
+    <br />
+    Wenn Sie mit der Ordination von Dr. Olivia telefonisch, per E-Mail oder per Formular auf der
+    Website Kontakt aufnehmen, werden Ihre angegebenen Daten nur für die Dauer unseres Kontaktes
+    gespeichert.
+    <br />
+    <br />
+    Ihre zur Verfügung gestellten und zur Erfüllung unserer Tätigkeiten notwendigen Daten werden
+    gemäß den gesetzlichen Aufbewahrungsfristen gespeichert bzw. aufbewahrt.
+    <br />
+    <br />
+    Sie haben das Recht jederzeit über den Inhalt und Umfang Ihrer gespeicherten Daten in der von
+    Ihnen gewünschten Art Auskunft zu erhalten.
+    <br />
+    <br />
+    Nach Ablauf der oben erwähnten Fristen haben Sie – sofern nicht gesetzliche Aufbewahrungsfristen
+    dagegen sprechen – die Möglichkeit, die Löschung aller bei der Ordination von Dr. Olivia
+    gespeicherten personenbezogenen Daten schriftlich anzufordern. Ein entsprechendes
+    Löschungsprotokoll wird Ihnen nach schriftlicher Aufforderung/Antrag zur Verfügung gestellt.
+    <br />
+    <br />
+
+    <span class="text-h4 block">Ihre Rechte</span>
+    Sie haben jederzeit das Recht auf Auskunft, Berichtigung, Löschung, Einschränkung,
+    Datenübertragbarkeit, Widerruf und Widerspruch. Sollten Sie der Meinung sein, dass die
+    Verarbeitung Ihrer Daten gegen die DSGVO verstößt oder Ihre datenschutzrechtlichen Ansprüche in
+    einer anderen Art und Weise verletzt wurden, ersuchen wir Sie um schriftliche Kontaktaufnahme.
+    <br />
+    <br />
+    Weitere Informationen finden Sie unter dem Link der zuständigen Behörde
+    <RouterLink to="https://www.dsb.gv.at/">https://www.dsb.gv.at/</RouterLink>
+    <br />
+    <br />
+    <span class="text-h6">Sie erreichen uns unter folgenden Kontaktdaten:</span>
+    <br />
+    <br />
+    <span class="text-weight-bold">Ordination Dr. Olivia</span> <br />
+    Dornbacher Straße 1, <br />
+    1170 Wien <br />
+    <br />
+    <span style="text-decoration: underline">+43 1 486 46 27</span> <br />
+    <span style="text-decoration: underline">office@zahnwien.at</span> <br />
+    <br />
+
+    <span class="text-h4 block">Für diese Website</span>
+    Mit dieser Datenschutzerklärung informieren wir Sie über die wichtigsten Aspekte der
+    Datenverarbeitung im Rahmen unserer Webseite. Wir erheben und verarbeiten personenbezogene Daten
+    nur auf Grundlage der gesetzlichen Bestimmungen (Datenschutzgrundverordnung,
+    Telekommunikationsgesetz 2003).
+    <br />
+    <br />
+    Sobald Sie als Benutzer auf unsere Webseite zugreifen oder diese besuchen wird Ihre IP-Adresse,
+    Beginn sowie Beginn und Ende der Sitzung erfasst. Dies ist technisch bedingt und stellt somit
+    ein berechtigtes Interesse iSv Art 6 Abs 1 lit f DSGVO dar.
+    <br />
+    <br />
+
+    <span class="text-h4 block">Kontakt mit uns</span>
+    Wenn Sie uns, entweder über unser Kontaktformular auf unserer Webseite, oder per Email
+    kontaktieren, dann werden die von Ihnen an uns übermittelten Daten zwecks Bearbeitung Ihrer
+    Anfrage oder für den Fall von weiteren Anschlussfragen für sechs Monate bei uns gespeichert. Es
+    erfolgt, ohne Ihre Einwilligung, keine Weitergabe Ihrer übermittelten Daten.
+    <br />
+    <br />
+
+    <span class="text-h4 block">Cookies</span>
+    Unsere Website verwendet so genannte Cookies. Dabei handelt es sich um kleine Textdateien, die
+    mit Hilfe des Browsers auf Ihrem Endgerät abgelegt werden. Sie richten keinen Schaden an. Wir
+    nutzen Cookies dazu, unser Angebot nutzerfreundlich zu gestalten. Einige Cookies bleiben auf
+    Ihrem Endgerät gespeichert, bis Sie diese löschen. Sie ermöglichen es uns, Ihren Browser beim
+    nächsten Besuch wiederzuerkennen. Wenn Sie dies nicht wünschen, so können Sie Ihren Browser so
+    einrichten, dass er Sie über das Setzen von Cookies informiert und Sie dies nur im Einzelfall
+    erlauben. Bei der Deaktivierung von Cookies kann die Funktionalität unserer Website
+    eingeschränkt sein. Google Fonts Unsere Website verwendet Schriftarten von „Google Fonts“. Der
+    Dienstanbieter dieser Funktion ist: • Google Ireland Limited Gordon House, Barrow Street Dublin
+    4. Ireland Tel: +353 1 543 1000 Beim Aufrufen dieser Webseite lädt Ihr Browser Schriftarten und
+    speichert diese in den Cache. Da Sie, als Besucher der Webseite, Daten des Dienstanbieters
+    empfangen kann Google unter Umständen Cookies auf Ihrem Rechner setzen oder analysieren. Die
+    Nutzung von „Google-Fonts“ dient der Optimierung unserer Dienstleistung und der einheitlichen
+    Darstellung von Inhalten. Dies stellt ein berechtigtes Interesse im Sinne von Art. 6 Abs. 1 lit.
+    f DSGVO dar. Weitere Informationen zu Google Fonts erhalten Sie unter folgendem Link:
+    https://developers.google.com/fonts/faq Weitere Informationen über den Umgang mit Nutzerdaten
+    von Google können Sie der Datenschutzerklärung entnehmen:
+    https://policies.google.com/privacy?hl=de . Google verarbeitet die Daten auch in den USA, hat
+    sich jedoch dem EU-US Privacy-Shield unterworfen. https://www.privacyshield.gov/EU-US-Framework
+    Server-Log Files Diese Webseite und der damit verbundene Provider erhebt im Zuge der
+    Webseitennutzung automatisch Informationen im Rahmen sogenannter „Server-Log Files“. Dies
+    betrifft insbesondere: • IP-Adresse oder Hostname • den verwendeten Browser • Land/Region aus
+    dem der Browserzugriff erfolgt • Aufenthaltsdauer auf der Webseite sowie Datum und Uhrzeit •
+    aufgerufene Seiten der Webseite • Spracheinstellungen und Betriebssystem • „Leaving-Page“ (auf
+    welcher URL hat der Benutzer die Webseite verlassen) • ISP (Internet Service Provider) Diese
+    erhobenen Informationen werden nicht personenbezogen verarbeitet oder mit personenbezogenen
+    Daten in Verbindung gebracht. Der Webseitenbetreiber behält es sich vor, im Falle von
+    Bekanntwerden rechtswidriger Tätigkeiten, diese Daten auszuwerten oder zu überprüfen. Newsletter
+    Sie haben die Möglichkeit, über unsere Website unseren Newsletter zu abonnieren. Hierfür
+    benötigen wir Ihre E-Mail-Adresse und ihre Erklärung, dass Sie mit dem Bezug des Newsletters
+    einverstanden sind. Wenn Sie die Möglichkeit nutzen unseren Newsletter zu abonnieren, wird Ihre
+    Email-Adresse und Ihre Zustimmung/Einverständniserklärung über den Bezug des Newsletters
+    benötigt. In Folge auf die Anmeldung zu unserem Newsletter, erhalten Sie von uns ein
+    Bestätigungs-Email mit einem Link zur Anmeldebestätigung. Eine Stornierung des Newsletters ist
+    jederzeit möglich. Ihre Daten im Zusammenhang mit dem Newsletter-Versand werden im Falle einer
+    Stornierung umgehend von uns gelöscht. Die Rechtmäßigkeit der erfolgten Datenverarbeitung bis
+    zum Widerruf, welche aufgrund der Zustimmung passiert ist, wird nicht berührt. Ihre Rechte als
+    Betroffener Sie als Betroffener haben bezüglich Ihrer Daten, welche bei uns gespeichert sind
+    grundsätzlich ein Recht auf: • Auskunft • Löschung der Daten • Berichtigung der Daten •
+    Übertragbarkeit der Daten • Wiederruf und Widerspruch zur Datenverarbeitung • Einschränkung Wenn
+    sie vermuten, dass im Zuge der Verarbeitung Ihrer Daten Verstöße gegen das Datenschutzrecht
+    passiert sind, so haben Sie die Möglichkeit sich bei der Ordination von Dr. Olivia oder der
+    Datenschutzbehörde zu beschweren. Diese Datenschutzerklärung wird von der Ordination Dr. Olivia
+    periodisch aktualisiert und auf Letztstand gebracht. Version 1.0 11/2025
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
