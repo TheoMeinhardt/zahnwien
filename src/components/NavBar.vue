783abc9c2a2b820b3b1c9f7a25a756776f987622
Theo Meinhardt <theo.meinhardt@batinfo.at>
2026-03-31 14:27:27 +0200

    Removed NavBar Item Standorte

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -18,7 +18,6 @@
         :aria-hidden="!navbarExpanded">
         <RouterLink to="/contactUs" class="col-2 text-weight-bolder">Terminvereinbarung</RouterLink>
         <RouterLink to="/aboutUs" class="col-auto">Über uns</RouterLink>
-        <RouterLink to="/" class="col-auto">Standorte</RouterLink>
         <RouterLink to="/galerie" class="col-auto">Galerie</RouterLink>
         <RouterLink to="/contactUs" class="col-auto">Kontakt</RouterLink>
       </nav>
