7a73e7506e1026e21669d17bd0d410518ced3979
Theo Meinhardt <theo.meinhardt@batinfo.at>
2026-03-31 13:39:00 +0200

    Removed DornbacherstraßeView and LandingView, updated routes accordingly

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import LandingView from '@/views/LandingView.vue'
-import DornbacherstrasseView from '@/views/DornbacherstrasseView.vue'
 import MariahilferstrasseView from '@/views/MariahilferstrasseView.vue'
 import AboutUsView from '@/views/AboutUsView.vue'
 import PrivacyPolicyView from '@/views/PrivacyPolicyView.vue'
@@ -14,20 +12,6 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      redirect: '/home',
-    },
-    {
-      path: '/home',
-      component: LandingView,
-      name: 'LandingView',
-    },
-    {
-      path: '/dornbacherstrasse',
-      component: DornbacherstrasseView,
-      name: 'DornbacherstrasseView',
-    },
-    {
-      path: '/mariahilferstrasse',
       component: MariahilferstrasseView,
       name: 'MariahilferstrasseView',
     },
